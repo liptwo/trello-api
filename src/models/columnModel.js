b4d03c138a9e38e8f630d1ebaee44cec836cce5f
liptwo <liptwo0@gmail.com>
2025-05-29 22:32:52 +0700

    api_create_board

--- src/models/columnModel.js
+++ src/models/columnModel.js
@@ -1,11 +1,13 @@
 import Joi from 'joi'
 import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
 
-const COLUMN_COLLECTION_NAME = 'boards'
+const COLUMN_COLLECTION_NAME = 'columns'
 const COLUMN_COLLECTION_SCHEMA = Joi.object({
   boardId: Joi.string().required().min(3).pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE),
   title: Joi.string().required().min(3).max(50).trim().strict(),
-  cardOrderIds: Joi.array().items(Joi.string()).default([]),
+  cardOrderIds: Joi.array().items(
+    Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE
+    )).default([]),
   createdAt: Joi.date().timestamp('javascript').default(Date.now),
   updatedAt: Joi.date().timestamp('javascript').default(null),
   _destroy: Joi.boolean().default(false)
--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-useless-catch */
-import ApiError from '~/utils/ApiError'
+import { boardModel } from '~/models/boardModel'
+// import ApiError from '~/utils/ApiError'
 import { slugify } from '~/utils/fomartter'
 
 const createNew = async ( reqBody ) => {
@@ -14,13 +15,17 @@ const createNew = async ( reqBody ) => {
 
     // gọi tới thằng model để xử lý lưu bản ghi vào db
     //...
+    const createdBoard = await boardModel.createNew(newBoard)
 
+    // lấy bảng ghi board sau khi gọi (tùy mục đích dự án)
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
 
+    // console.log(getNewBoard)
     // Làm thêm các xử lý logic khác với các collection khác tùy đặc thù dự án... v.v
     // bắn email, notification về cho admin khi có 1 board mới được tạo.. v.v
 
 
-    return newBoard
+    return getNewBoard
   } catch (error) { throw error }
 }
 
